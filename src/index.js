5c691722b63cc93a4ab151783ce6f668ba4b8ebd
lance thibodeau <heyimlance@gmail.com>
2019-03-24 19:30:24 -0700

    add basic styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import MapboxClient from "mapbox";
 import { WebMercatorViewport } from "viewport-mercator-project";
+import "./styles.css";
 
 class Geocoder extends Component {
     debounceTimeout = null;
